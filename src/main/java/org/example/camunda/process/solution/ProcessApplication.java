a28b8e2cf693369aadf6560f17f112a938cc2e04
Falko Menge <falko.menge@camunda.com>
2022-04-25 14:57:47 +0200

    Add process application with REST facade

---
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -0,0 +1,17 @@
+package org.example.camunda.process.solution;
+
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableZeebeClient
+@ZeebeDeployment(resources = "classpath*:/models/*.*")
+public class ProcessApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProcessApplication.class, args);
+  }
+	
+}
