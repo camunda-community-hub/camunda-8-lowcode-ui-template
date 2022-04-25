32aa43232cf981cfc3459ea9ff21cb4e8d8d2f48
Falko Menge <falko.menge@camunda.com>
2022-04-25 14:58:06 +0200

    Add mock service + worker

---
+++ src/main/java/org/example/camunda/process/solution/service/MockService.java
@@ -0,0 +1,13 @@
+package org.example.camunda.process.solution.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MockService {
+
+    public void invoke(String businessKey) throws JsonProcessingException {
+    }
+
+}
