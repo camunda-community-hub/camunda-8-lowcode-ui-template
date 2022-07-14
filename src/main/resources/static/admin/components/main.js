1797156a6c828ad5df27838c7f9c75fb147ce9ad
Christophe <christophe.dame@camunda.com>
2022-07-14 11:55:51 +0200

    Add the possibility to add new forms

--- src/main/resources/static/admin/components/main.js
+++ src/main/resources/static/admin/components/main.js
@@ -24,20 +24,15 @@ Vue.component('main-page',{
 		schema = {
 		  "components": [
 			{
-			  "text": "<img src=\"http://localhost:8080/camunda/online/banking/img/core-img/logo.png\"><h2>Instructions:</h2><p>Based on internal protocal and the given information below, tick the checkbox for 'Approved' if this application should be approved.</p><p> Otherwise, leave this checkbox blank.</p>",
-			  "type": "text",
-			  "id": "Field_0e9521u"
-			},
-			{
-			  "key": "applicationNumber",
-			  "label": "Application Number",
+			  "key": "sometext",
+			  "label": "Some Text",
 			  "type": "textfield",
 			  "id": "Field_1djmro0"
 			},
 		  ],
 		  "schemaVersion": 4,
 		  "type": "default",
-		  "id": "Form_0840o2b",
+		  "id": "Form_"+Math.floor(1000000 + Math.random() * 9000000),
 		  "executionPlatform": "Camunda Cloud",
 		  "executionPlatformVersion": "1.1",
 		  "exporter": {
@@ -45,28 +40,28 @@ Vue.component('main-page',{
 			"version": "5.0.0"
 		  }
 		}
-		console.log(schema);
 
 		this.$store.formEditor = new FormEditor.FormEditor({
 		  container: document.querySelector('#form-editor')
 		});
-		console.log(this.$store.formEditor);
 		this.$store.formEditor.importSchema(schema);
 	}
 });
 
 Vue.component('my-menu',{ template: '<div class="dropdown">'+
 			 ' <button class="btn btn-outline-light dropdown-toggle" type="button" id="menuEditor" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Camunda Form Editor</button>'+
-			  '<div class="dropdown-menu" aria-labelledby="menuEditor">'+
+			  '<div class="dropdown-menu" aria-labelledby="menuEditor" style="width:280px">'+
 				'<div class="dropdown-item">'+
 					'<div class="input-group">'+
-						'<span class="input-group-text"><i class="bi bi-file-earmark-code"></i></span>'+
 						'<input type="text" class="form-control" placeholder="Form name" aria-label="Formname" v-model="$store.form.name">'+
 						'<button class="btn btn-sm btn-primary" v-on:click="saveForm()">Save</button>'+
 					'</div>'+
 				'</div>'+
+				'<div class="dropdown-item">'+
+					'<button class="btn btn-block btn-primary" v-on:click="newForm()">New Form</button>'+
+				'</div>'+
 				'<div class="btn-group dropend">'+
-				  '<button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">'+
+				  '<button type="button" class="btn btn-block dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">'+
 					'Load '+
 				  '</button>'+
 				  '<ul class="dropdown-menu">'+
@@ -81,6 +76,31 @@ Vue.component('my-menu',{ template: '<div class="dropdown">'+
     }
   },
   methods: {
+	newForm() {
+		schema = {
+		  "components": [
+			{
+			  "key": "sometext",
+			  "label": "Some Text",
+			  "type": "textfield",
+			  "id": "Field_1djmro0"
+			},
+		  ],
+		  "schemaVersion": 4,
+		  "type": "default",
+		  "id": "Form_"+Math.floor(1000000 + Math.random() * 9000000),
+		  "executionPlatform": "Camunda Cloud",
+		  "executionPlatformVersion": "1.1",
+		  "exporter": {
+			"name": "Camunda Modeler",
+			"version": "5.0.0"
+		  }
+		}
+		this.$store.form.schema = schema;
+		this.$store.form.id=null;
+		this.$store.form.name='New Form';
+		this.$store.formEditor.importSchema(schema);
+	},
 	saveForm() {
 		this.$store.form.schema = this.$store.formEditor.getSchema();
 		axios.post('/edition/forms', this.$store.form).then(response => {
@@ -96,9 +116,7 @@ Vue.component('my-menu',{ template: '<div class="dropdown">'+
 			this.$store.form.name = form.name;
 			this.$store.form.schema = form.schema;
 			this.$store.formEditor.importSchema(form.schema);
-			console.log(this.$store.form);
 		}).catch(error => {
-		console.log(error);
 			alert(error.message); 
 		})
 	}
