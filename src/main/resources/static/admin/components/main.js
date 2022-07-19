eec1782bb790b4339bc9c0889829eca3a7b99874
Christophe <christophe.dame@camunda.com>
2022-07-19 10:48:48 +0200

    Download and upload forms

--- src/main/resources/static/admin/components/main.js
+++ src/main/resources/static/admin/components/main.js
@@ -1,11 +1,23 @@
 Vue.component('my-header',{ template: '<nav class="navbar bg-dark">'+
 		'<div class="container-fluid">'+
-			'<my-menu></my-menu>'+
+			'<div class="row"><my-menu></my-menu>'+
+			'<div class="col input-group">'+
+						'<input type="text" class="form-control" placeholder="Form name" aria-label="Formname" v-model="$store.form.name">'+
+						'<button class="btn btn-sm btn-primary" v-on:click="saveForm()">Save</button>'+
+					'</div></div>'+
 			'<div><span class="text-light"><b>{{$store.user.name}}</b> working on <b>{{$store.form.name}}</b></span> '+
 			' <a class="logout bi bi-box-arrow-left text-light" @click="logout()"></a></div>'+
 		'</div>'+
 	'</nav>',
   methods: {
+	saveForm() {
+		this.$store.form.schema = this.$store.formEditor.getSchema();
+		axios.post('/edition/forms', this.$store.form).then(response => {
+			this.$store.form.id = response.data.id; 
+		}).catch(error => {
+			alert(error.message); 
+		})
+	},
 	  logout() {
 		this.$store.user.name=null;
 		this.$store.user.token=null;
@@ -19,6 +31,7 @@ Vue.component('my-header',{ template: '<nav class="navbar bg-dark">'+
 Vue.component('main-page',{
   template: '<div><my-header></my-header>'+
 		'<div id="form-editor"></div>'+
+		'<upload-form-modal></upload-form-modal>'+
 	'</div>',
 	mounted: function(){
 		schema = {
@@ -48,18 +61,9 @@ Vue.component('main-page',{
 	}
 });
 
-Vue.component('my-menu',{ template: '<div class="dropdown">'+
+Vue.component('my-menu',{ template: '<div class="col dropdown">'+
 			 ' <button class="btn btn-outline-light dropdown-toggle" type="button" id="menuEditor" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Camunda Form Editor</button>'+
 			  '<div class="dropdown-menu" aria-labelledby="menuEditor" style="width:280px">'+
-				'<div class="dropdown-item">'+
-					'<div class="input-group">'+
-						'<input type="text" class="form-control" placeholder="Form name" aria-label="Formname" v-model="$store.form.name">'+
-						'<button class="btn btn-sm btn-primary" v-on:click="saveForm()">Save</button>'+
-					'</div>'+
-				'</div>'+
-				'<div class="dropdown-item">'+
-					'<button class="btn btn-block btn-primary" v-on:click="newForm()">New Form</button>'+
-				'</div>'+
 				'<div class="btn-group dropend">'+
 				  '<button type="button" class="btn btn-block dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">'+
 					'Load '+
@@ -68,6 +72,11 @@ Vue.component('my-menu',{ template: '<div class="dropdown">'+
 					'<li v-for="name in names"><a class="dropdown-item" v-on:click="loadForm(name)">{{name}}</a></li>'+
 				  '</ul>'+
 				'</div>'+
+				'<a class="dropdown-item" v-on:click="newForm()"><i class="bi bi-plus-square"></i> New Form</a>'+
+				'<a class="dropdown-item" v-on:click="duplicate()"><i class="bi bi-files"></i> Duplicate</a>'+
+				'<li><hr class="dropdown-divider"></li>'+
+				'<a class="dropdown-item" v-on:click="download()"><i class="bi bi-box-arrow-down"></i> Download</a>'+
+				'<a class="dropdown-item" v-on:click="upload()"><i class="bi bi-box-arrow-in-up"></i> Load from file</a>'+
 			  '</div>'+
 			'</div>',
   data: function () {
@@ -101,13 +110,10 @@ Vue.component('my-menu',{ template: '<div class="dropdown">'+
 		this.$store.form.name='New Form';
 		this.$store.formEditor.importSchema(schema);
 	},
-	saveForm() {
-		this.$store.form.schema = this.$store.formEditor.getSchema();
-		axios.post('/edition/forms', this.$store.form).then(response => {
-			this.$store.form.id = response.data.id; 
-		}).catch(error => {
-			alert(error.message); 
-		})
+	duplicate() {
+		this.$store.form.id=null;
+		this.$store.form.schema.id = "Form_"+Math.floor(1000000 + Math.random() * 9000000);
+		this.$store.form.name='Duplicate '+this.$store.form.name;
 	},
 	loadForm(name) {
 		axios.get('/edition/forms?name='+name).then(response => {
@@ -119,6 +125,20 @@ Vue.component('my-menu',{ template: '<div class="dropdown">'+
 		}).catch(error => {
 			alert(error.message); 
 		})
+	},
+	download() {
+		let url = window.URL.createObjectURL(new Blob([JSON.stringify(this.$store.form.schema, null, 2)], {type: "application/json"}));
+	    const a = document.createElement('a');
+	    a.style.display = 'none';
+	    a.href = url;
+	    a.download = this.$store.form.name+".form";
+	    a.click();
+	    window.URL.revokeObjectURL(url);
+	    a.remove();
+	},
+	upload() {
+		let modal = new bootstrap.Modal(document.getElementById('upload-form-modal'), {});
+		modal.show();
 	}
   },
   created: function () {
@@ -127,5 +147,62 @@ Vue.component('my-menu',{ template: '<div class="dropdown">'+
 	}).catch(error => {
 		alert(error.message); 
 	})
+  }
+});
+
+
+Vue.component('upload-form-modal',{
+  template: '<div class="modal fade" id="upload-form-modal" ref="upload-form-modal" tabindex="-1">'+
+  '<div class="modal-dialog">'+
+    '<div class="modal-content">'+
+      '<div class="modal-header bg-secondary text-light">'+
+        '<h5 class="modal-title">Upload a form file</h5>'+
+        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'+
+      '</div>'+
+      '<div class="modal-body">'+
+		'<div class="mb-3">'+
+		  '<label for="formFile" class="form-label">Form file</label>'+
+		  '<input class="form-control" type="file" id="formFile">'+
+		'</div>'+
+      '</div>'+
+      '<div class="modal-footer">'+
+        '<button type="button" :disabled="data.value == \'\'" class="btn btn-primary" @click="load" data-bs-dismiss="modal">Load</button>'+
+        '<button class="btn btn-link" data-bs-dismiss="modal">Cancel</button>'+
+      '</div>'+
+    '</div>'+
+  '</div>'+
+'</div>',
+  data() {
+    return {
+		data: {
+		  value:""
+		}
+	}
+  },
+  methods: {
+	  prepareLoad(){
+		var file = document.getElementById('formFile').files[0];
+		if (file) {
+		  var reader = new FileReader();
+		  reader.readAsText(file);
+		  let _this = this;
+		  reader.onload = function(e) {
+			console.log(_this);
+		    _this.data.value = e.target.result;                     
+		  };
+		}
+	  },
+	  load() {
+		let schema = JSON.parse(this.data.value);
+		this.$store.form.id = null;
+		this.$store.form.name = schema.id;
+		this.$store.form.schema = schema;
+		this.$store.formEditor.importSchema(this.$store.form.schema);
+		let modal = new bootstrap.Modal(document.getElementById('upload-form-modal'), {});
+		modal.hide();
+	  }
+  },
+  mounted(){
+	document.getElementById("formFile").addEventListener('change', this.prepareLoad);
   }
 });
