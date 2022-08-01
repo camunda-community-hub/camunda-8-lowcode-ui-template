d81a9427b4accaa73e509388326db5c35d7c797e
Christophe <christophe.dame@camunda.com>
2022-08-01 12:25:37 +0200

    Make the project compatible with local camunda setup with identity and keycloak

--- src/main/java/org/example/camunda/process/solution/service/OperateService.java
+++ src/main/java/org/example/camunda/process/solution/service/OperateService.java
@@ -1,6 +1,12 @@
 package org.example.camunda.process.solution.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import io.camunda.operate.CamundaOperateClient;
+import io.camunda.operate.auth.LocalIdentityAuthentication;
 import io.camunda.operate.auth.SaasAuthentication;
 import io.camunda.operate.dto.ProcessDefinition;
 import io.camunda.operate.exception.OperateException;
@@ -8,35 +14,50 @@ import io.camunda.operate.search.ProcessDefinitionFilter;
 import io.camunda.operate.search.SearchQuery;
 import io.camunda.operate.search.Sort;
 import io.camunda.operate.search.SortOrder;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
 @Service
 public class OperateService {
 
-  @Value("${zeebe.client.cloud.client-id}")
+  @Value("${zeebe.client.cloud.client-id:notProvided}")
   private String clientId;
 
-  @Value("${zeebe.client.cloud.client-secret}")
+  @Value("${zeebe.client.cloud.client-secret:notProvided}")
   private String clientSecret;
 
-  @Value("${zeebe.client.cloud.clusterId}")
+  @Value("${zeebe.client.cloud.clusterId:notProvided}")
   private String clusterId;
 
-  @Value("${zeebe.client.cloud.region}")
+  @Value("${zeebe.client.cloud.region:notProvided}")
   private String region;
+
+  @Value("${identity.clientId:notProvided}")
+  private String identityClientId;
+
+  @Value("${identity.clientSecret:notProvided}")
+  private String identityClientSecret;
+
+  @Value("${operateUrl:notProvided}")
+  private String operateUrl;
 
   private CamundaOperateClient client;
 
-  private CamundaOperateClient getCamundaTaskListClient() throws OperateException {
+  private CamundaOperateClient getCamundaOperateClient() throws OperateException {
     if (client == null) {
-      SaasAuthentication sa = new SaasAuthentication(clientId, clientSecret);
-      client =
-          new CamundaOperateClient.Builder()
-              .operateUrl("https://" + region + ".operate.camunda.io/" + clusterId)
-              .authentication(sa)
-              .build();
+      if (!"notProvided".equals(clientId)) {
+        SaasAuthentication sa = new SaasAuthentication(clientId, clientSecret);
+        client =
+            new CamundaOperateClient.Builder()
+                .operateUrl("https://" + region + ".operate.camunda.io/" + clusterId)
+                .authentication(sa)
+                .build();
+      } else {
+        LocalIdentityAuthentication la =
+            new LocalIdentityAuthentication()
+                .clientId(identityClientId)
+                .clientSecret(identityClientSecret);
+        client =
+            new CamundaOperateClient.Builder().operateUrl(operateUrl).authentication(la).build();
+      }
     }
     return client;
   }
@@ -50,6 +71,6 @@ public class OperateService {
             .withSort(new Sort("version", SortOrder.DESC))
             .build();
 
-    return getCamundaTaskListClient().searchProcessDefinitions(procDefQuery);
+    return getCamundaOperateClient().searchProcessDefinitions(procDefQuery);
   }
 }
