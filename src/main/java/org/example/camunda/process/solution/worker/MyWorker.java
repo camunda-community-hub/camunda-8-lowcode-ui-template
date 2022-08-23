49c1204e6d4e660c4df4320fb04f75756b9ceff7
Dave Paroulek <david.paroulek@camunda.com>
2022-08-23 10:09:33 -0400

    accept java auto formatting changes

--- src/main/java/org/example/camunda/process/solution/worker/MyWorker.java
+++ src/main/java/org/example/camunda/process/solution/worker/MyWorker.java
@@ -2,10 +2,13 @@ package org.example.camunda.process.solution.worker;
 
 import io.camunda.zeebe.spring.client.annotation.ZeebeVariablesAsType;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import java.util.List;
 import org.example.camunda.process.solution.ProcessVariables;
-import org.example.camunda.process.solution.service.MyService;
+import org.example.camunda.process.solution.model.User;
+import org.example.camunda.process.solution.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,16 +16,15 @@ public class MyWorker {
 
   private static final Logger LOG = LoggerFactory.getLogger(MyWorker.class);
 
-  private final MyService myService;
+  @Autowired private UserService userService;
 
-  public MyWorker(MyService myService) {
-    this.myService = myService;
-  }
-
-  @ZeebeWorker(type = "my-service", autoComplete = true)
+  @ZeebeWorker(type = "selectAssignee", autoComplete = true)
   public ProcessVariables invokeMyService(@ZeebeVariablesAsType ProcessVariables variables) {
     LOG.info("Invoking myService with variables: " + variables);
 
-    return new ProcessVariables(); // new object to avoid sending unchanged variables
+    List<User> users = userService.all();
+    int idx = (int) Math.floor(Math.random() * users.size());
+
+    return new ProcessVariables().setAssignee1(users.get(idx).getUsername());
   }
 }
