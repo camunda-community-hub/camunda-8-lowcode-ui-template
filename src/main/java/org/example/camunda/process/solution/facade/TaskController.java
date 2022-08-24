5cd00220b099805211c12ff5b70359c87cacd709
Dave Paroulek <david.paroulek@camunda.com>
2022-08-24 16:14:06 -0400

    Allow cross origin requests to rest endpoints

--- src/main/java/org/example/camunda/process/solution/facade/TaskController.java
+++ src/main/java/org/example/camunda/process/solution/facade/TaskController.java
@@ -10,13 +10,9 @@ import org.example.camunda.process.solution.service.TaskListService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
