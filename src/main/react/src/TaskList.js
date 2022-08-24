a329dcc1091b3d7893a4765f1978211d5fe274f7
Dave Paroulek <david.paroulek@camunda.com>
2022-08-24 15:29:27 -0400

    Latest react js updates

--- src/main/react/src/TaskList.js
+++ src/main/react/src/TaskList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Stomp} from '@stomp/stompjs';
+import { Form } from '@bpmn-io/form-js-viewer';
 
 const rest = require('rest');
 const mime = require('rest/interceptor/mime');
@@ -10,12 +11,15 @@ const baseUrl = 'localhost:8080';
 const restUrl = `http://${baseUrl}`;
 const sockUrl = `ws://${baseUrl}/ws`;
 
+let merge = (a, b) => ({...a,...b});
+
 class AppScreen extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       user: this.props.user,
+      selectedTask: null,
       screen: "waiting"
     };
   }
@@ -31,11 +35,6 @@ class AppScreen extends Component {
       //this.startProcess(userId);
     }
   }
-
-  onProcessDefClick = (e) => {
-    this.props.onProcessDefClick(e);
-  };
-
   getProcesses = () => {
     client(`${restUrl}/process/definition/latest`).then((response) => {
       //console.log("getProcesses");
@@ -73,17 +72,17 @@ class AppScreen extends Component {
     });
   }
 
+  onTaskClick = (task) => (e) => {
+    this.clearSelectedTask();
+    this.onUserTaskReady(task);
+  }
+
   wsConnect = () => {
     stompClient = Stomp.client(sockUrl);
-    //let onGetTasks = this.onGetTasks.bind(this);
-    //let getTasks = this.getTasks.bind(this);
-    //let userId = this.state.user.userId;
-    //let onFormReady = this.onFormReady.bind(this);
+    let userId = this.state.user.username;
+    let onUserTaskReady = this.onUserTaskReadyWS.bind(this);
     stompClient.onConnect = function(frame){
-      //stompClient.subscribe('/topic/process/'+userId, onProcessEvent);
-      //stompClient.subscribe('/topic/tasks/'+userId, onGetTasks);
-      //stompClient.subscribe('/topic/forms/'+userId, onFormReady);
-      //getTasks();
+      stompClient.subscribe("/topic/" + userId + "/userTask", onUserTaskReady);
     };
 
     stompClient.onStompError =function(frame) {
@@ -97,8 +96,78 @@ class AppScreen extends Component {
     stompClient.send(endpoint, {}, JSON.stringify(json));
   }
 
+  onUserTaskReadyWS = (message) => {
+    let task = JSON.parse(message.body);
+    this.onUserTaskReady(task);
+  }
+
+  onUserTaskReady = (task) => {
+    //console.log(task);
+    this.setState({selectedTask: task});
+    this.setState({screen: "loadForm"});
+  }
+
+  clearSelectedTask = () => {
+    // remove selected Task
+    this.setState({selectedTask: null});
+
+    // remove form from dom
+    const container = document.querySelector('#form');
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+
+  onFormSubmit = (e, results) => {
+    let { data, errors } = results;
+    if (Object.keys(errors).length) {
+      console.error('Form has errors', errors);
+    } else {
+      console.log(data);
+      this.setState({screen: "waiting"});
+      // merge variables
+      let variables = merge(this.state.selectedTask.variables, results.data);
+      this.completeTask(this.state.selectedTask.id, variables);
+      this.clearSelectedTask();
+    }
+  }
+
+  completeTask = (taskId, variables) => {
+    client({
+      path: `${restUrl}/tasks/${taskId}`,
+      headers: {'Content-Type': 'application/json'},
+      entity: variables
+    }).then((response) => {
+      this.getAssignedTasks();
+      this.getUnassignedTasks();
+    });
+  }
+
   componentDidMount() {
     this.init();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if(this.state.screen === "loadForm") {
+      console.log("Attempt to load form");
+
+      this.setState({screen: "form"});
+
+      // Load the form
+      const container = document.querySelector('#form');
+      if(!container.firstChild) {
+        const bpmnForm = new Form({container: container});
+        bpmnForm.on('submit', this.onFormSubmit);
+        let schema = JSON.parse(this.state.selectedTask.formSchema);
+        let variables = this.state.selectedTask.variables;
+        bpmnForm.importSchema(schema, variables).then(
+          function (result) {
+            //console.log(result);
+          });
+
+        this.setState({bpmnForm: bpmnForm});
+      }
+    }
   }
 
   render() {
@@ -107,11 +176,17 @@ class AppScreen extends Component {
         <div className={"column is-2 is-card"}>
           <aside className="menu">
             <p className="menu-label">
-              Tasks
+              My Tasks
             </p>
             <ul className="menu-list">
-              {this.state.tasks ? this.state.tasks.map((task) =>
-                <li key={task.id}><a onClick={this.onTaskClick}>{task.processName} - {task.name} [ {task.id} ]</a></li>
+              {this.state.assignedTasks ? this.state.assignedTasks.map((task) =>
+                <li key={task.id}>
+                  <a id={task.id} onClick={this.onTaskClick(task)}>
+                    <span className={"is-size-5 has-text-weight-medium"}>{task.name}</span><br/>
+                    <span className={"is-size-6 has-text-weight-light"}>{task.processName}</span><br/>
+                    <span className={"is-size-7 has-text-weight-light"}>{task.creationTime}</span>
+                </a>
+                </li>
               ) : "(No Tasks Found)"}
             </ul>
             <p className="menu-label">
@@ -132,7 +207,7 @@ class AppScreen extends Component {
         <div className={"column"}>
           <section className="section">
             <div className="container">
-              {this.props.children}
+              <div id={"form"}></div>
             </div>
           </section>
         </div>
