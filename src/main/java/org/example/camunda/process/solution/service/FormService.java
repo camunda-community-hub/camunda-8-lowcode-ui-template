e045b09ac9ea605942e3323ca44cb5ba696f96ae
Dave Paroulek <david.paroulek@camunda.com>
2022-08-24 16:16:02 -0400

    Add utility methods for reading form schema out of bpmn
    This is useful if you want to maintain form schemas inside the bpmn file.
    
    But since this lowcode project reads form schemas from file system,
    this probably isn't needed. I added it here in case this code is useful
    in the future.

--- src/main/java/org/example/camunda/process/solution/service/FormService.java
+++ src/main/java/org/example/camunda/process/solution/service/FormService.java
@@ -4,16 +4,29 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import org.example.camunda.process.solution.facade.dto.Form;
 import org.example.camunda.process.solution.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 @Service
 public class FormService {
@@ -22,6 +35,34 @@ public class FormService {
 
   @Value("${workspace:workspace}")
   private String workspace;
+
+  public String parseFormIdFromKey(String formKey) {
+    Pattern pattern = Pattern.compile("[^:]+:[^:]+:(.*)");
+    Matcher matcher = pattern.matcher(formKey);
+    while (matcher.find()) {
+      return matcher.group(1);
+    }
+    return null;
+  }
+
+  public String getFormSchemaFromBpmn(String bpmnFileName, String formId)
+      throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
+    DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = builderFactory.newDocumentBuilder();
+    // TODO: this is used by the reactjs app. Need to integrate this
+    InputStream bpmnIs =
+        this.getClass().getClassLoader().getResourceAsStream("/models/" + bpmnFileName);
+    Document xmlDocument = builder.parse(bpmnIs);
+    XPath xPath = XPathFactory.newInstance().newXPath();
+    String expression = "//*[@id=\"" + formId + "\"]";
+    NodeList nodeList =
+        (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+    if (nodeList != null && nodeList.getLength() == 1) {
+      return nodeList.item(0).getFirstChild().getNodeValue();
+    } else {
+      throw new IllegalStateException("Unable to find json schema for form with name " + formId);
+    }
+  }
 
   public Path resolveForm(String name) {
     return Path.of(workspace).resolve(FORMS).resolve(name);
