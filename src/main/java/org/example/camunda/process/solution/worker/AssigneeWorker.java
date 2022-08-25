a312ab0053ca34418122f23ccfdfc339c4b2d033
Falko Menge <falko.menge@camunda.com>
2022-08-25 14:28:26 +0200

    Restore template worker and service

---
+++ src/main/java/org/example/camunda/process/solution/worker/AssigneeWorker.java
@@ -0,0 +1,32 @@
+package org.example.camunda.process.solution.worker;
+
+import java.util.List;
+
+import org.example.camunda.process.solution.ProcessVariables;
+import org.example.camunda.process.solution.model.User;
+import org.example.camunda.process.solution.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.camunda.zeebe.spring.client.annotation.ZeebeVariablesAsType;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+
+@Component
+public class AssigneeWorker {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MyWorker.class);
+
+  @Autowired private UserService userService;
+
+  @ZeebeWorker(type = "selectAssignee", autoComplete = true)
+  public ProcessVariables selectAssignee(@ZeebeVariablesAsType ProcessVariables variables) {
+    LOG.info("Invoking myService with variables: " + variables);
+
+    List<User> users=userService.all();
+    int idx = (int) Math.floor(Math.random()*users.size());
+
+    return new ProcessVariables().setAssignee1(users.get(idx).getUsername());
+  }
+}
--- src/main/java/org/example/camunda/process/solution/worker/MyWorker.java
+++ src/main/java/org/example/camunda/process/solution/worker/MyWorker.java
@@ -1,32 +1,28 @@
 package org.example.camunda.process.solution.worker;
 
-import java.util.List;
-
-import org.example.camunda.process.solution.ProcessVariables;
-import org.example.camunda.process.solution.model.User;
-import org.example.camunda.process.solution.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import io.camunda.zeebe.spring.client.annotation.ZeebeVariablesAsType;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import org.example.camunda.process.solution.ProcessVariables;
+import org.example.camunda.process.solution.service.MyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Component
 public class MyWorker {
 
   private static final Logger LOG = LoggerFactory.getLogger(MyWorker.class);
 
-  @Autowired private UserService userService;
+  private final MyService myService;
 
-  @ZeebeWorker(type = "selectAssignee", autoComplete = true)
+  public MyWorker(MyService myService) {
+    this.myService = myService;
+  }
+
+  @ZeebeWorker(type = "my-service", autoComplete = true)
   public ProcessVariables invokeMyService(@ZeebeVariablesAsType ProcessVariables variables) {
     LOG.info("Invoking myService with variables: " + variables);
 
-    List<User> users=userService.all();
-    int idx = (int) Math.floor(Math.random()*users.size());
-
-    return new ProcessVariables().setAssignee1(users.get(idx).getUsername());
+    return new ProcessVariables(); // new object to avoid sending unchanged variables
   }
 }
