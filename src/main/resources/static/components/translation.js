98effcc309b3c43ec6c83728f3dd5e9e1c99894f
Falko Menge <falko.menge@camunda.com>
2022-08-25 14:30:10 +0200

    Add de translation, make en default, add missing

--- src/main/resources/static/components/translation.js
+++ src/main/resources/static/components/translation.js
@@ -1,9 +1,11 @@
 const messages = {
   en: {
     message: {
+	  tasks: 'Tasks',
 	  mytasks:'My tasks',
 	  unassignedtasks:'Unassigned tasks',
 	  archivedtasks:'My closed tasks',
+	  processes: 'Processes',
 	  newInstance:'New instance',
 	  instanceConfirmation:'Process instance started',
 	  open:'Open',
@@ -13,11 +15,29 @@ const messages = {
       hello: 'Camunda'
     }
   },
+  de: {
+    message: {
+	  tasks: 'Aufgaben',
+	  mytasks:'Meine Aufgaben',
+	  unassignedtasks:'Offene Aufgaben',
+	  archivedtasks:'Abgeschlossene Aufgaben',
+	  processes: 'Prozesse',
+	  newInstance:'Neue Instanz',
+	  instanceConfirmation:'Prozessinstanz gestartet',
+	  open:'Öffnen',
+	  submit:'Absenden',
+	  claim: 'Übernehmen',
+	  unclaim: 'Abgeben',
+      hello: 'Camunda'
+    }
+  },
   fr: {
     message: {
+	  tasks:'Tâches',
 	  mytasks:'Mes tâches',
 	  unassignedtasks:'Tâches non assignées',
 	  archivedtasks:'Mes tâches archivées',
+	  processes: 'Procédures',
 	  newInstance:'Nouvelle procédure',
 	  instanceConfirmation:'Procédure démarrée',
 	  open:'Ouvrir',
@@ -31,7 +51,7 @@ const messages = {
 
 // Create VueI18n instance with options
 const i18n = new VueI18n({
-  locale: 'fr', // set locale
+  locale: 'en', // set locale
   messages, // set locale messages
 })
 
@@ -43,6 +63,9 @@ Vue.component('translate-menu',{ template: '<div class="dropdown localeDropdown"
 					'<a @click="english()">English</a>'+
 				'</div>'+
 				'<div class="dropdown-item">'+
+					'<a @click="german()">Deutsch</a>'+
+				'</div>'+
+				'<div class="dropdown-item">'+
 					'<a @click="french()">Français</a>'+
 				'</div>'+
 			  '</div>'+
@@ -50,6 +73,9 @@ Vue.component('translate-menu',{ template: '<div class="dropdown localeDropdown"
   methods: {
     english() {
 		i18n.locale='en';
+    },
+    german() {
+		i18n.locale='de';
     },
     french() {
 		i18n.locale='fr';
