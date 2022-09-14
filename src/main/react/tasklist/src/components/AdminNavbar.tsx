80e308a36bb19112d7e574a41ae41ea9afe15e49
Christophe <christophe.dame@camunda.com>
2022-09-14 11:24:39 +0200

    draft on preview

--- src/main/react/tasklist/src/components/AdminNavbar.tsx
+++ src/main/react/tasklist/src/components/AdminNavbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import authService from '../service/AuthService';
@@ -7,24 +7,32 @@ import logo from '../assets/img/logo.svg'
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import DataPreviewEditor from './DataPreviewEditor';
 
 function AdminNavbar() {
   const user = useSelector((state: any) => state.auth.data)
   const form = useSelector((state: any) => state.adminForms.currentForm)
   const dispatch = useDispatch();
 
+  const [showPreview, setShowPreview] = useState(false);
+
+  const handleClose = () => setShowPreview(false);
+  const handleShow = () => setShowPreview(true);
+
   const logout = (event: any) => {
     dispatch(authService.signOut());
   };
 
   return (
+    <>
     <nav className="navbar navbar-dark bg-dark">
       <div className="container-fluid">
         <img width="140" src={logo} className="custom-logo" alt="Camunda" />
         <div>
           {form ?
             <InputGroup className="mb-3">
-              <Button variant="primary"><i className="bi bi-eye"></i></Button>
+              <Button variant="primary" onClick={handleShow}><i className="bi bi-eye"></i></Button>
               <Form.Control aria-label="Form name" value={form.name} onChange={(evt) => form.name = evt.target.name}/>
               <Button variant="primary" onClick={() => adminFormService.saveCurrentForm()}>Save</Button>
               <Button variant="secondary" onClick={() => dispatch(adminFormService.setForm(null))}><i className="bi bi-arrow-return-left"></i> Back</Button>
@@ -39,7 +47,24 @@ function AdminNavbar() {
           }
         </div>
       </div>
-    </nav>
+      </nav>
+      <Modal show={showPreview} onHide={handleClose} fullscreen>
+        <Modal.Header closeButton>
+          <Modal.Title>Preview form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="card col"><DataPreviewEditor/></div>
+            <div className="card col"><div id="task-form-preview"></div></div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </>
   );
 }
 
