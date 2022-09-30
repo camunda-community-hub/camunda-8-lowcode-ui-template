7700bf9989add7e5f57ca2013da6784fba8ed35a
McAlm <stefan.wiese@camunda.com>
2022-09-30 18:48:21 +0200

    allow external BPMN resources (SaaS)

--- src/main/java/org/example/camunda/process/solution/service/OperateService.java
+++ src/main/java/org/example/camunda/process/solution/service/OperateService.java
@@ -9,11 +9,17 @@ import io.camunda.operate.search.ProcessDefinitionFilter;
 import io.camunda.operate.search.SearchQuery;
 import io.camunda.operate.search.Sort;
 import io.camunda.operate.search.SortOrder;
+import java.util.AbstractMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableCaching
 public class OperateService {
 
   @Value("${zeebe.client.cloud.client-id:notProvided}")
@@ -74,5 +80,46 @@ public class OperateService {
             .build();
 
     return getCamundaOperateClient().searchProcessDefinitions(procDefQuery);
+  }
+
+  public List<ProcessDefinition> getProcessDefinitionByKey(Long key) throws OperateException {
+    ProcessDefinitionFilter processDefinitionFilter = new ProcessDefinitionFilter.Builder().build();
+    SearchQuery procDefQuery =
+        new SearchQuery.Builder()
+            .withFilter(processDefinitionFilter)
+            .withSize(1000)
+            .withSort(new Sort("version", SortOrder.DESC))
+            .build();
+
+    return getCamundaOperateClient().searchProcessDefinitions(procDefQuery);
+  }
+
+  @Cacheable("processXmls")
+  public String getProcessDefinitionXmlByKey(Long key) throws OperateException {
+    System.out.println("Entering getProcessDefinitionXmlByKey for key " + key);
+    List<ProcessDefinition> processDefs = getProcessDefinitions();
+    Map<Object, Object> processXmls;
+    processXmls =
+        processDefs.stream()
+            .map(
+                processDef -> {
+                  try {
+                    AbstractMap.SimpleEntry<Long, String> entry =
+                        new AbstractMap.SimpleEntry<>(
+                            processDef.getKey(),
+                            getCamundaOperateClient().getProcessDefinitionXml(processDef.getKey()));
+                    return entry;
+
+                  } catch (OperateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    return new AbstractMap.SimpleEntry<>(key, "NOT FOUND");
+                  }
+                })
+            .collect(
+                Collectors.toMap(
+                    AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
+
+    return processXmls.get(key).toString();
   }
 }
--- src/test/java/org/example/camunda/process/solution/BpmnUtilsTest.java
+++ src/test/java/org/example/camunda/process/solution/BpmnUtilsTest.java
@@ -3,27 +3,42 @@ package org.example.camunda.process.solution;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 import org.example.camunda.process.solution.utils.BpmnUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BpmnUtilsTest {
 
+  private String XML;
+
+  /**
+   * @throws IOException
+   */
+  @BeforeEach
+  public void setup() throws IOException {
+    ClassLoader classLoader = getClass().getClassLoader();
+    File file = new File(classLoader.getResource("models/simple-screenflow.bpmn").getFile());
+    XML = FileUtils.readFileToString(file, "UTF-8");
+  }
+
   @Test
   public void testGetTaskNameFromBpmn() {
-    String taskName = BpmnUtils.getTaskNameFromBpmn("simple-screenflow.bpmn", "Activity_1vqalfh");
+    String taskName = BpmnUtils.getTaskNameFromBpmn(XML, "Activity_1vqalfh");
     assertEquals("First Name", taskName);
   }
 
   @Test
   public void testGetFormSchemaFromBpmnFile() {
-    String schema =
-        BpmnUtils.getFormSchemaFromBpmnFile("simple-screenflow.bpmn", "userTaskForm_userTask1");
+    String schema = BpmnUtils.getFormSchemaFromBpmnFile(XML, "userTaskForm_userTask1");
     assertNotNull(schema);
   }
 
   @Test
   public void testGetProcessName() {
-    String processName = BpmnUtils.getProcessName("simple-screenflow.bpmn", "simple-screenflow");
+    String processName = BpmnUtils.getProcessName(XML, "simple-screenflow");
     assertEquals("simple-screenflow", processName);
   }
 }
