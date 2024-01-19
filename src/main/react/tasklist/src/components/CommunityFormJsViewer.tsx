ca0cd5da73481c8177d9082b052d7194a21f35ad
Christophe <christophe.dame@camunda.com>
2024-01-19 13:32:21 +0100

    resolve issue loading forms

--- src/main/react/tasklist/src/components/CommunityFormJsViewer.tsx
+++ src/main/react/tasklist/src/components/CommunityFormJsViewer.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import {useDispatch, useSelector} from 'react-redux';
-import type {} from 'redux-thunk/extend-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import type { } from 'redux-thunk/extend-redux';
 import taskService from '../service/TaskService';
 import processService from '../service/ProcessService';
 import { Form } from '@camunda-community/form-js-viewer';
@@ -15,55 +15,58 @@ function CommunityFormJsViewer(formViewer: IFormViewer) {
   const tasklistConf = useSelector((state: any) => state.process.tasklistConf)
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const container = document.querySelector('#task-form');
-  let errors:string[] = [];
-  let bpmnForm: Form = new Form({ container: container });
-  useLayoutEffect(() => {
-	if (container && formViewer.schema) {
-	  container.innerHTML = '';
+  let errors: string[] = [];
+  let bpmnForm: Form | null = null;
+
+  useEffect(() => {
+    const container = document.querySelector('#task-form');
+    if (container && formViewer.schema) {
+      container.innerHTML = '';
 
       bpmnForm = new Form({ container: container });
       if (formViewer.disabled) {
         bpmnForm.setProperty('readOnly', true);
       }
-	  bpmnForm.importSchema(formViewer.schema, formViewer.variables).then(
-		function (result: any) {
-		  console.log(result);
-		});
-	}
-  });
+      bpmnForm.importSchema(formViewer.schema, formViewer.variables).then(
+        function (result: any) {
+          console.log(result);
+        });
+    }
+  }, []);
 
 
   const submit = () => {
-	bpmnForm.validate();
+    if (bpmnForm != null) {
+      bpmnForm.validate();
 
-	for (const field in bpmnForm._getState().errors) {
-	  if (bpmnForm._getState().errors[field].length > 0) {
-		Array.prototype.push.apply(errors, bpmnForm._getState().errors[field]);
-	  }
-	}
-    if (errors.length == 0) {
-      if (formViewer.variables) {
-        if (!tasklistConf.splitPage) {
-          dispatch(taskService.submitTask(bpmnForm._getState().data, navigate("/tasklist")));
-        } else {
-          dispatch(taskService.submitTask(bpmnForm._getState().data));
+      for (const field in bpmnForm._getState().errors) {
+        if (bpmnForm._getState().errors[field].length > 0) {
+          Array.prototype.push.apply(errors, bpmnForm._getState().errors[field]);
         }
-      } else {
-        dispatch(processService.instantiate(bpmnForm._getState().data));
       }
-	}
+      if (errors.length == 0) {
+        if (formViewer.variables) {
+          if (!tasklistConf.splitPage) {
+            dispatch(taskService.submitTask(bpmnForm._getState().data, navigate("/tasklist")));
+          } else {
+            dispatch(taskService.submitTask(bpmnForm._getState().data));
+          }
+        } else {
+          dispatch(processService.instantiate(bpmnForm._getState().data));
+        }
+      }
+    }
   }
 
   return (
     <div>
-	  <div id="task-form"></div>
-	  <div className="ms-2 me-2 mb-2 d-flex justify-content-between">
+      <div id="task-form"></div>
+      <div className="ms-2 me-2 mb-2 d-flex justify-content-between">
         <button disabled={formViewer.disabled} type="button" className="btn btn-primary" onClick={submit}><i className="bi bi-send"></i> {t("Submit")}</button>
-	  </div>
-	</div>
+      </div>
+    </div>
   )
-  
+
 }
 
 export default CommunityFormJsViewer;
